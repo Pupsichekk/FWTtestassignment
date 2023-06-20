224147009d07e958551422e87450b141f366ae36
Pupsichekk <www.sopin-l@mail.ru>
2023-06-20 17:22:04 +0400

    Fixed borders on select inputs

--- src/helpers/IsEmpty.js
+++ src/helpers/IsEmpty.js
@@ -1,3 +1,4 @@
+// Helper for checking filters object in store, if values are empty = object is empty.
 export const isEmpty = (obj) => {
   const filteredValues = Object.values(obj).filter((value) => {
     if (value === "") return false;
