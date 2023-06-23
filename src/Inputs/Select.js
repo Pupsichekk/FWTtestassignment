0459449ff0d1eb29f7631204c7dc44ec70399a72
Pupsichekk <www.sopin-l@mail.ru>
2023-06-23 04:29:27 +0400

    Changed svg on select to fit figma markup

--- src/Inputs/Select.js
+++ src/Inputs/Select.js
@@ -1,10 +1,27 @@
 import React from "react";
-import Select from "react-select";
+import Select, { components } from "react-select";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./Select.module.scss";
 
 const Selects = (props) => {
+  const DropdownIndicator = (props) => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <svg
+          className={styles.svg}
+          width="10"
+          height="6"
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9.67861 1.8337L5.77064 5.68539C5.34503 6.10487 4.65497 6.10487 4.22936 5.68539L0.321394 1.8337C-0.365172 1.15702 0.121082 -8.36609e-08 1.09203 0L8.90797 6.7345e-07C9.87892 7.57111e-07 10.3652 1.15702 9.67861 1.8337Z" />
+        </svg>
+      </components.DropdownIndicator>
+    );
+  };
+
   const theme = useSelector((state) => state.theme.theme);
   const componentStyles = {
     control: (baseStyles, state) => {
@@ -71,12 +88,30 @@ const Selects = (props) => {
         paddingRight: "15px",
       };
     },
+    indicatorSeparator: (baseStyles) => {
+      return {
+        ...baseStyles,
+        height: 0,
+      };
+    },
+    dropdownIndicator: (baseStyles) => ({
+      ...baseStyles,
+      stroke: `${theme === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(0, 0, 0, 0.3)"}`,
+      fill: `${theme === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(0, 0, 0, 0.3)"}`,
+      paddingRight: "18px",
+    }),
   };
 
   return (
     <>
-      <SelectAuthor isClearable={true} styles={componentStyles} authorOnChangeHandler={props.authorOnChangeHandler} />
+      <SelectAuthor
+        components={{ DropdownIndicator }}
+        isClearable={true}
+        styles={componentStyles}
+        authorOnChangeHandler={props.authorOnChangeHandler}
+      />
       <SelectLocation
+        components={{ DropdownIndicator }}
         isClearable={true}
         styles={componentStyles}
         locationOnChangeHandler={props.locationOnChangeHandler}
@@ -107,6 +142,7 @@ const SelectAuthor = (props) => {
       options={authors}
       placeholder="Author"
       styles={props.styles}
+      components={props.components}
     />
   );
 };
@@ -133,6 +169,7 @@ const SelectLocation = (props) => {
       options={locations}
       placeholder="Location"
       styles={props.styles}
+      components={props.components}
     />
   );
 };
