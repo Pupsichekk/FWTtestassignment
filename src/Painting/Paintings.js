820a89e173ba3bbfa08bacf773098410553e756e
Pupsichekk <www.sopin-l@mail.ru>
2023-06-25 10:23:40 +0400

    Fixed small issue

--- src/Painting/Paintings.js
+++ src/Painting/Paintings.js
@@ -5,10 +5,13 @@ import React from "react";
 const Paintings = (props) => {
   return (
     <div className={styles.grid}>
-      {props.items.length === 0 && <div>No items found :&#60; </div>}
-      {props.items.map((painting) => {
-        return <Painting key={painting.id} painting={painting} />;
-      })}
+      {props.items.length === 0 ? (
+        <div>No items found :&#60; </div>
+      ) : (
+        props.items.map((painting) => {
+          return <Painting key={painting.id} painting={painting} />;
+        })
+      )}
     </div>
   );
 };
